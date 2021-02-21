912102f1d63e1b8d08a4fade79953c77873bf195
Caresle <carabesle@gmail.com>
2021-02-20 20:37:08 -0700

    End theme switcher, desktop version

--- js/app.js
+++ js/app.js
@@ -7,10 +7,12 @@ themeSwitcher.addEventListener('click', () => {
   if(darkTheme) {
     body.classList.add('light')
     body.classList.remove('dark')
+    themeSwitcher.childNodes[0].classList.add('floating-selector')
     darkTheme = false
   } else {
     body.classList.add('dark')
     body.classList.remove('light')
+    themeSwitcher.childNodes[0].classList.remove('floating-selector')
     darkTheme = true
   }
 })
