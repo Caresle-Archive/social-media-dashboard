7ed470525262dba5b64cfd709e9f804812653c7f
Caresle <carabesle@gmail.com>
2021-02-20 19:32:35 -0700

    Add media queries

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
-const body = document.getElementById('body')
-const dashboard = document.getElementById('dashboard-container')
+const body = document.body
+const themeSwitcher = document.getElementById('theme-switcher')
+
+let darkTheme = true
+
+themeSwitcher.addEventListener('click', () => {
+  if(darkTheme) {
+    body.classList.add('light')
+    body.classList.remove('dark')
+    darkTheme = false
+  } else {
+    body.classList.add('dark')
+    body.classList.remove('light')
+    darkTheme = true
+  }
+})
